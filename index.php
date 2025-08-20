44cfb9eec5867b57a974061f9ea6dbbd459e2292
Haritha Dushmantha <121777418+devdush@users.noreply.github.com>
2025-08-20 11:24:05 +0530

    Php file created

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+// Sample PHP code for testing
+
+echo "Hello, World!<br>";
+
+$number = 42;
+echo "The number is: " . $number . "<br>";
+
+$array = ["apple", "banana", "cherry"];
+foreach ($array as $fruit) {
+    echo "Fruit: $fruit<br>";
+}
+
+// Simple function
+function add($a, $b) {
+    return $a + $b;
+}
+
+echo "Sum of 5 and 7 is: " . add(5, 7) . "<br>";
+?>
